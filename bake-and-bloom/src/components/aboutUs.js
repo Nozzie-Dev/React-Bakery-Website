7588452d49bd32edc30f104fe8759ed53cafd80a
Noziphezinhle Nzimande <125486198+Nozzie-Dev@users.noreply.github.com>
2024-08-06 22:18:37 +0200

    export about us to component

---
+++ bake-and-bloom/src/components/aboutUs.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const AboutUs = () => (
+      <div className="text-center p-4">
+        <h1 className="text-4xl font-bold mb-4">About Us</h1>
+        <p className="text-lg">
+          Welcome to our bakery! 
+          Remember banana bread quarentine?! Well our founder did not stop there.
+          You can now enjoy a well crafted range of baked good. Perfect to special events
+          or personal indulgance. <span>Have your cake, and eat it too!</span></p>
+  
+          <h2 className="text-2xl font-bold style-italic">Coming Soon...</h2>
+          <p className="text-lg">
+            The brain child of a true girlies girl, we will be bringing you beautifuly curated 
+            and customized floral arrangements. To meet your every need, sharing love, romance,
+            adoration or just random spoils. Let us deliver beautiful blooms to you and yours.
+          </p>
+      </div>
+    );
+  
+export default AboutUs;
